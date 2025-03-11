0360cfd232f6635b74e47609332c9d3dc331e1a9
Haokun Tian <tianhaokun258@gmail.com>
2025-03-11 23:20:23 +0000

    update plot

--- scripts/plot_true_dissim.py
+++ scripts/plot_true_dissim.py
@@ -9,21 +9,24 @@ true_dissim = get_true_dissim()
 for d, mtx in true_dissim.items():
     true_dissim[d] = min_max_normalization(mask(mtx)).numpy()
 
-sparse_mtx = block_diag(*list(true_dissim.values()))
+idx1, idx2, idx3 = 0, 10, 20
+dataset_names = list(true_dissim.keys())
+matrices = list(true_dissim.values())
+sparse_mtx = block_diag(matrices[idx1], matrices[idx2], matrices[idx3])
+block_labels = [dataset_names[idx1], dataset_names[idx2], dataset_names[idx3]]
+block_sizes = [matrices[idx1].shape[0], matrices[idx2].shape[0], matrices[idx3].shape[0]]
+starts = np.cumsum([0] + block_sizes[:-1])
+
 rows, cols = sparse_mtx.nonzero()
 values = sparse_mtx[rows, cols]
 x = rows + cols
 y = cols - rows
 
-block_labels = list(true_dissim.keys())
-block_sizes = [m.shape[0] for m in true_dissim.values()]
-starts = np.cumsum([0] + block_sizes[:-1])
-
-plt.figure(figsize=(12, 3))
-scatter = plt.scatter(x, y, c=values, cmap="viridis", marker="s", s=1)
+plt.figure(figsize=(6, 2))
+scatter = plt.scatter(x, y, c=values, cmap="viridis", marker="s", s=5)
 
 y_min = np.min(y)
-label_y_position = y_min - 5  # Vertical position for labels
+label_y_position = y_min - 2  # Vertical position for labels
 
 for label, start, size in zip(block_labels, starts, block_sizes):
 
@@ -35,7 +38,7 @@ for label, start, size in zip(block_labels, starts, block_sizes):
         label,
         ha="center",
         va="top",
-        rotation=90,
+        rotation=0,
         fontsize=8,
         color="black",
     )
