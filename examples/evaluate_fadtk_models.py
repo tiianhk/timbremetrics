cd52011e7b37252c4057303400709a84eafc534c
Haokun Tian <tianhaokun258@gmail.com>
2025-03-12 09:47:09 +0000

    update for examples

--- examples/evaluate_fadtk_models.py
+++ examples/evaluate_fadtk_models.py
@@ -1,21 +1,30 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+import torch
 from fadtk.model_loader import *
 from timbremetrics.metrics import TimbreMetric
-import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 models = [
     CLAPModel("2023"),
-    CLAPLaionModel("audio"),
-    CLAPLaionModel("music"),
+    CLAPLaionModel("audio"), CLAPLaionModel("music"),
+    VGGishModel(),
+    MERTModel(layer=12),
     EncodecEmbModel("24k"),
     EncodecEmbModel("48k"),
     DACModel(),
     CdpamModel("acoustic"),
     CdpamModel("content"),
-    W2V2Model("base", layer=10),
-    HuBERTModel("base", layer=10),
-    WavLMModel("base", layer=10),
+    W2V2Model('base', layer=12),
+    W2V2Model('large', layer=24),
+    HuBERTModel('base', layer=12),
+    HuBERTModel('large', layer=24),
+    WavLMModel('base', layer=12),
+    WavLMModel('base-plus', layer=12),
+    WavLMModel('large', layer=24),
+    WhisperModel('tiny'), WhisperModel('small'),
+    WhisperModel('base'), WhisperModel('medium'),
+    WhisperModel('large'),
 ]
 
 res = {}
@@ -32,5 +41,6 @@ for model in models:
         res[model.name] = metric()
     except Exception as e:
         err[model.name] = str(e)
+    torch.cuda.empty_cache()
 print(res)
 print(err)
